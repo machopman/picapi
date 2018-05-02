3ffef3916a0f47fe110e87914dd41c901d53e4e6
macho <machopman_shop@hotmail.com>
2018-05-02 18:20:22 +0700

    wefwefwe

--- web/index.php
+++ web/index.php
@@ -1,14 +1,15 @@
 <?php
 
-if(!empty($_GET['id'])) {
-    $url = "http://mandm.plearnjai.com/API/detailMovie.php?idmovie=" . $_GET['id'];
-    $json = file_get_contents($url);
-    $array = json_decode($json, true);
-    $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
+//if(!empty($_GET['id'])) {
+    //$url = "http://mandm.plearnjai.com/API/detailMovie.php?idmovie=" . $_GET['id'];
+    $url="https://raw.githubusercontent.com/kittinan/Sample-Line-Bot/master/images/beer.jpg";
+    #$json = file_get_contents($url);
+    #$array = json_decode($json, true);
+    #$urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
 
     //$imageData = base64_encode(file_get_contents($urlpic));
     //echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 
-}
+//}
 ?>
-<img src='<?php echo $urlpic;?>' alt="Girl in a jacket" style="width:239px;height:239px;">
+<img src='<?php echo $url;?>' alt="Girl in a jacket" style="width:239px;height:239px;">
