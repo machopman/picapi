17a4001e9e44caa2a32174b9205e82df30cb2a7e
macho <machopman_shop@hotmail.com>
2018-05-02 15:50:31 +0700

    dzdcx

--- web/index.php
+++ web/index.php
@@ -1,6 +1,14 @@
 <?php
+if(!empty($_GET['id'])) {
+    $url = "http://mandm.plearnjai.com/API/detailMovie.php?idmovie=".$_GET['id'];
+    $json = file_get_contents($url);
+    $array =json_decode($json,true);
+    $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
+    $imageData = base64_encode(file_get_contents($urlpic));
+    echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 
-$image = 'http://www.majorcineplex.com/uploads/movie/1792/thumb_1792.jpg';
-$imageData = base64_encode(file_get_contents($image));
-echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
-?>
+
+
+
+
+}
