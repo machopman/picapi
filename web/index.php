7b87f7c0efaef56da5e8e6ff7ea134b9767b0256
macho <machopman_shop@hotmail.com>
2018-05-02 16:57:50 +0700

    5t5t

--- web/index.php
+++ web/index.php
@@ -1,13 +1,24 @@
 <?php
-
+header('Content-type: image/jpeg');
 
 if(!empty($_GET['id'])) {
     $url = "http://mandm.plearnjai.com/API/detailMovie.php?idmovie=" . $_GET['id'];
     $json = file_get_contents($url);
     $array = json_decode($json, true);
     $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
-    $imageData = base64_encode(file_get_contents($urlpic));
-    echo '<img src="data:image/jpeg;base64,' . $imageData . '">';
+   // $imageData = base64_encode(file_get_contents($urlpic));
+    $percent = 0.2; // percentage of resize
+    // Get new dimensions
+    list($width, $height) = getimagesize($urlpic);
+    $new_width = $width * $percent;
+    $new_height = $height * $percent;
 
+    // Resample
+    $image_p = imagecreatetruecolor($new_width, $new_height);
+    $image = imagecreatefromjpeg($urlpic);
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    // Output
+    imagejpeg($image_p, null, 100);
 }
 ?>
