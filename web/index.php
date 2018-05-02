ad66291d4236dbe7d4fe99e55d5ad19d7098e7dc
macho <machopman_shop@hotmail.com>
2018-05-02 15:56:00 +0700

    swsw

--- web/index.php
+++ web/index.php
@@ -1,15 +1,15 @@
 <?php
-if(!empty($_GET['id'])) {
-    $url = "http://mandm.plearnjai.com/API/detailMovie.php?idmovie=".$_GET['id'];
-    $json = file_get_contents($url);
-    $array =json_decode($json,true);
-    $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
-    $imageData = base64_encode(file_get_contents($urlpic));
-    
-    echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 
 
+echo 'HelloWorld';
+
+//if(!empty($_GET['id'])) {
+ //   $url = "http://mandm.plearnjai.com/API/detailMovie.php?idmovie=".$_GET['id'];
+  //  $json = file_get_contents($url);
+  //  $array =json_decode($json,true);
+  //  $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
+ //   $imageData = base64_encode(file_get_contents($urlpic));
+   // echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 
 
-
-}
+?>
