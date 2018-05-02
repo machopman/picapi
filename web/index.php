50589e7a7587946ffda633d87db247abacd4f6b3
macho <machopman_shop@hotmail.com>
2018-05-02 17:26:25 +0700

    3ewqedqw

--- web/index.php
+++ web/index.php
@@ -6,19 +6,7 @@ if(!empty($_GET['id'])) {
     $json = file_get_contents($url);
     $array = json_decode($json, true);
     $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
-   // $imageData = base64_encode(file_get_contents($urlpic));
-   // $percent = 0.2; // percentage of resize
-    // Get new dimensions
-    list($width, $height) = getimagesize($urlpic);
-    // = $width * $percent;
-   // $new_height = $height * $percent;
-
-    // Resample
-    $image_p = imagecreatetruecolor(239, 239);
-    $image = imagecreatefromjpeg($urlpic);
-    imagecopyresampled($image_p, $image, 0, 0, 0, 0, 239, 239, $width, $height);
-
-    // Output
-    imagejpeg($image_p, null, 100);
+    $imageData = base64_encode(file_get_contents($urlpic));
+    echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 }
 ?>
