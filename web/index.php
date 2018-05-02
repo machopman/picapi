5af20641130092c1fd06fc76470bbc19606af4be
macho <machopman_shop@hotmail.com>
2018-05-02 17:50:52 +0700

    cdcdc

--- web/index.php
+++ web/index.php
@@ -5,7 +5,10 @@ if(!empty($_GET['id'])) {
     $json = file_get_contents($url);
     $array = json_decode($json, true);
     $urlpic = $array['response'][0]['detailMovie'][0]['Poster'];
-    $imageData = base64_encode(file_get_contents($urlpic));
-    echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
+
+    //$imageData = base64_encode(file_get_contents($urlpic));
+    //echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
+
 }
 ?>
+<img src='<?php echo $urlpic;?>' alt="Girl in a jacket" style="width:239px;height:239px;">
